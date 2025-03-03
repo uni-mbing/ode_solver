9be60ab2bb2895a2f45810f70d7c9919bbdaa822
uni-mbing <max.bing.mb@gmail.com>
2025-03-03 18:30:07 +0100

    implement cli app interface

--- src/ode_solver.cpp
+++ src/ode_solver.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 #include <Problem.h>
 #include <Solver.h>
+#include <CLI/CLI.hpp>
 
 typedef Vector<double> vec;
 
+enum SOLVER_TYPE{
+    RUNGE_KUTTA_4,
+    LEAP_FROG,
+    EE
+};
+
+enum SIMULATION_TYPE{
+    LINEAR_ODE,
+    N_BODY
+};
+
 int main(int argc, char** argv){
+    CLI::App app{"ODE Solver App"};
+
+    // Definiere eine optionale Eingabe für eine Zahl
+    std::string solverType = "RK4";
+    app.add_option("-s,--solver", solverType, "Input solver type: RUNGE_KUTTA_4, LEAP_FROG, EE");
+
+    // Definiere eine optionale Eingabe für einen Namen
+    std::string problemType;
+    app.add_option("-p,--problem", problemType, "Input problem type: LINEAR, N_BODY");
+
+    // Parse die Kommandozeilenargumente
+    CLI11_PARSE(app, argc, argv);
+
+    std::cout << problemType << " " << solverType << std::endl;
+
     std::vector<double> init ={2};
     vec y0(init);
 
@@ -16,7 +43,7 @@ int main(int argc, char** argv){
     std::unique_ptr<ODE_Problem<double>> problem = std::make_unique<Linear_ODE>(y0, t0, lambda);
     RK4<double> solver(std::move(problem), stepSize, maxIterations);
 
-    solver.solve();
+    // solver.solve();
 
 
     return 0;
